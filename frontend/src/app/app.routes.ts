668bb0ab3e9a9e52e987c4a3d15eb5eba7067d43
Charmy <104645298+Charmying@users.noreply.github.com>
2026-01-05 00:06:56 +0800

    feat(frontend): add home page entry points

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { NormalLayoutComponent } from './core/normal-layout/normal-layout.component';
+import { HomeComponent } from './features/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: NormalLayoutComponent,
+    children: [
+      {
+        path: '',
+        component: HomeComponent
+      }
+    ]
+  }
+];
