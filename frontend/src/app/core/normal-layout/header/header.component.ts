5dfff25f1a821b9bf61dd265623b8881b3bfc584
Charmy <104645298+Charmying@users.noreply.github.com>
2026-01-05 15:34:18 +0800

    feat(frontend): introduce i18n infrastructure

--- frontend/src/app/core/normal-layout/header/header.component.ts
+++ frontend/src/app/core/normal-layout/header/header.component.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+import { LanguageService } from '../../services/language.service';
 
 @Component({
   selector: 'app-header',
   standalone: true,
+  imports: [TranslateModule],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  protected lang = inject(LanguageService);
+}
