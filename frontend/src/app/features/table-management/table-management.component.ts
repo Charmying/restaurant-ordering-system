de3ed831cb08a2864c2158487e869c7af1b31b75
Charmy <104645298+Charmying@users.noreply.github.com>
2026-01-16 02:27:26 +0800

    feat(frontend): add dashboard fatures page

---
+++ frontend/src/app/features/table-management/table-management.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-table-management',
+  standalone: true,
+  imports: [],
+  templateUrl: './table-management.component.html',
+  styleUrls: ['./table-management.component.scss'],
+})
+export class TableManagementComponent {}
